dda3844d97618dc5839db1b86675bdeaf47e2f5e
Anil Kumar <akt01.dev@gmail.com>
2024-10-11 13:23:08 +0530

    feat: add ViewTransition & Input components

---
+++ web/src/lib/components/Input.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	const { type, ...rest } = $props();
+	import { Eye, EyeClosed } from 'phosphor-svelte';
+
+	let showPassword = $state(false);
+</script>
+
+<div class="input-wrapper" class:pw={type === 'password'}>
+	<input
+		type={type === 'password' ? (showPassword ? 'text' : 'password') : (type as string)}
+		{...rest}
+	/>
+	{#if type === 'password'}
+		<button type="button" onclick={() => (showPassword = !showPassword)} class="icon">
+			{#if showPassword}
+				<Eye />
+			{:else}
+				<EyeClosed />
+			{/if}
+		</button>
+	{/if}
+</div>
+
+<style>
+	.input-wrapper {
+		background: #3b364c;
+		overflow: hidden;
+		gap: var(--spacing-12);
+		display: flex;
+		align-items: center;
+		border-radius: var(--rounded-4);
+
+		&:focus-within {
+			border: 2px solid #746898;
+		}
+
+		input {
+			padding: var(--spacing-12);
+			min-width: 0;
+			width: 100%;
+		}
+
+		&.pw {
+			padding-right: var(--spacing-12);
+		}
+
+		.icon {
+			color: #a8a8a8;
+			cursor: pointer;
+		}
+	}
+</style>
---
+++ web/src/lib/components/Navigation.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+                resolve();
+				await navigation.complete;
+			});
+		});
+	});
+</script>
